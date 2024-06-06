5370cd0224573cced00e37a52f4dafef7080277a
Lars Zinnkann <Lars.Zinnkann@kamax.com>
2024-06-06 21:14:38 +0200

    Moin

--- db.py
+++ db.py
@@ -7,11 +7,32 @@ mydb = mysql.connector.connect(
   database="Test"
 )
 
+Hersteller="Test"
+bezeichnung="keta"
+Beschreibungstext="guter akku"
+Listenpreis=999.00
+Rahmenhöhe="27"
+Gewicht=19.00
+Schaltungstyp="Kettenschaltung"
+Bremsentyp="Scheibenbremse"
+farbe="schwarz"
+Fahrradtyp="3"
+
 mycursor = mydb.cursor()
 
-sql = "UPDATE `fahrrad` SET `Bezeichnung` = 'REACTION HYBRID ABS 75' WHERE `fahrrad`.`ArtikelID` = 1"
+lang_bezeichnung= Hersteller + " " + bezeichnung + ", " + farbe + ", " + Rahmenhöhe
 
-mycursor.execute(sql)
+fahrrad_einfuegen = "Insert INTO fahrrad (Hersteller, Bezeichnung, lang_bezeichnung, Beschreibungstext, Listenpreis) Values (%s, %s, %s, %s, %s)"
+
+mycursor.execute(fahrrad_einfuegen, (Hersteller, bezeichnung, lang_bezeichnung, Beschreibungstext, Listenpreis))
+
+ID_abfrage = "Select ArtikelID from fahrrad where lang_bezeichnung = %s"
+
+mycursor.execute(ID_abfrage, (lang_bezeichnung,))
+id = mycursor.fetchone()
+
+fahrradmerkmale = "Insert INTO fahrradmerkmale (ArtikelID, Rahmenhöhe, Gewicht, Schaltungstyp, Bremsentyp, farbe, FahrradtypID) Values (%s, %s, %s , %s, %s, %s, %s)"
+mycursor.execute(fahrradmerkmale, (id, Rahmenhöhe, Gewicht, Schaltungstyp, Bremsentyp, farbe, Fahrradtyp))
 
 mydb.commit()
 
